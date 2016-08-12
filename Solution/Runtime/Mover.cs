5d86e49b0f9171bdf6364343c8697e736d8acaea
northsteel <vitalym87@gmail.com>
2016-08-12 20:56:43 +0600

    Finished "Create a player and its enemies" part. Changed code structure. Modified .gitignore file.

---
+++ Solution/Runtime/Mover.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Moves game object in specified direction
+/// </summary>
+public class Mover : MonoBehaviour
+{
+    #region FIELDS
+
+    /// <summary>
+    /// Object speed
+    /// </summary>
+    [SerializeField]
+    private Vector2 speed = new Vector2(10f, 10f);
+
+    /// <summary>
+    /// Moveing direction
+    /// </summary>
+    [SerializeField]
+    private Vector2 direction = new Vector2(-1f, 0f);
+
+    /// <summary>
+    /// Reference to Rigidbody2D component
+    /// </summary>
+    private Rigidbody2D rb;
+
+    /// <summary>
+    /// Stores movement values
+    /// </summary>
+    private Vector2 movement;
+
+    #endregion
+
+    #region PROPERTIES
+
+
+
+    #endregion
+
+    #region METHODS
+
+    private void Awake()
+    {
+        if (!(rb = GetComponent<Rigidbody2D>()))
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>();
+        }
+
+    }
+
+    private void Update()
+    {
+        // Movement per direction
+        movement = new Vector2(Time.deltaTime * speed.x * direction.x, Time.deltaTime * speed.y * direction.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement;
+    }
+
+    #endregion
+}
---
+++ Solution/Runtime/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    #region FIELDS
+
+    /// <summary>
+    /// The speed of the ship
+    /// </summary>
+    [SerializeField]
+    private Vector2 force = new Vector2(50f, 50f);
+
+    /// <summary>
+    /// Reference to Rigidbody2D component
+    /// </summary>
+    private Rigidbody2D rb;
+
+    /// <summary>
+    /// Stores the movement
+    /// </summary>
+    private Vector2 movement;
+
+    #endregion
+
+    #region PROPERTIES
+
+
+
+    #endregion
+
+    #region METHODS
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        // Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // Movement per direction
+        movement = new Vector2(Time.deltaTime * force.x * inputX, Time.deltaTime * force.y * inputY);
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddForce(movement); 
+        }
+    }
+
+    #endregion
+}
